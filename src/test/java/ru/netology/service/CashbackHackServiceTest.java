3ce23bab9dd466c546736d383e7261a3320d13fd
Rodionova Evgenia <jenechkajj353@gmail.com>
2021-06-22 23:47:55 +0300

    testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,30 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+
+    @Test
+    public void shouldShowIfUnder1000() {
+        int amount = 700;
+
+        int actual = service.remain( amount );
+        int expected = 300;
+
+        assertEquals( actual, expected );
+    }
+
+    @Test
+    public void shouldShowIfOver1000() {
+        int amount = 1500;
+
+        int actual = service.remain( amount );
+        int expected = 500;
+
+        assertEquals( actual, expected );
+    }
+}
