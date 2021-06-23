9f0edcbbe095cdd7612097405ac382b877478401
EvgeniaRodi <80852346+EvgeniaRodi@users.noreply.github.com>
2021-06-23 23:17:22 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,24 +7,23 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-
     @Test
     public void shouldShowIfUnder1000() {
         int amount = 700;
 
-        int actual = service.remain( amount );
+        int actual = service.remain(amount);
         int expected = 300;
 
-        assertEquals( actual, expected );
+        assertEquals(actual, expected);
     }
 
     @Test
     public void shouldShowIfOver1000() {
         int amount = 1500;
 
-        int actual = service.remain( amount );
+        int actual = service.remain(amount);
         int expected = 500;
 
-        assertEquals( actual, expected );
+        assertEquals(actual, expected);
     }
 }
