1b9751c8b079a4425a874950234a484851880ce8
EvgeniaRodi <80852346+EvgeniaRodi@users.noreply.github.com>
2021-06-23 23:19:02 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -11,28 +11,29 @@ public class CashbackHackServiceTest {
     public void shouldShowIfUnder1000() {
         int amount = 700;
 
-        int actual = service.remain( amount );
+        int actual = service.remain(amount);
         int expected = 300;
 
-        assertEquals( actual, expected );
+        assertEquals(actual, expected);
     }
 
     @Test
     public void shouldShowIfOver1000() {
         int amount = 1500;
 
-        int actual = service.remain( amount );
+        int actual = service.remain(amount);
         int expected = 500;
 
-        assertEquals( actual, expected );
+        assertEquals(actual, expected);
     }
+    
     @Test
     public void shouldShow1000() {
         int amount = 1000;
 
-        int actual = service.remain( amount );
+        int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals( actual, expected );
+        assertEquals(actual, expected);
     }
 }
