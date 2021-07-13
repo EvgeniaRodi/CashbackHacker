fdc3b0de3424045c24cf9ad21e83fda242244cc8
EvgeniaRodi <80852346+EvgeniaRodi@users.noreply.github.com>
2021-07-13 23:52:37 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -26,5 +26,14 @@ public class CashbackHackServiceTest {
 
         assertEquals(actual, expected);
     }
- 
+    
+  @Test
+    public void shouldShowIfEqual1000() {
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
 }
